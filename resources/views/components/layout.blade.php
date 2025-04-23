0cf3a2b3e762bd7e5e1efd2673c43ab0e21be0eb
emiliano53 <emiliano.santos.flores@gmail.com>
2025-04-23 01:21:47 -0600

    layout

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <link rel="stylesheet" href={{ URL::asset('bootstrap-5.3.3-dist/css/bootstrap.min.css')}} />
+    <link rel="stylesheet" href={{ URL::asset('bootstrap-5.3.3-dist/css/bootstrap.min.css') }} />
     <script src={{ URL::asset('bootstrap-5.3.3-dist/js/bootstrap.min.js') }}></script>
     <link href={{ URL::asset('DataTables/datatables.css') }} rel="stylesheet"/>
     <script src={{ URL::asset('DataTables/datatables.js') }}></script>
@@ -10,21 +10,26 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mobile Hub</title>
 </head>
-
-    <body>
-        <div class="row">
-            <div class="col-2">
-            @component("components.sidebar")
-            @endcomponent
+<body style="height: 100vh; margin: 0;">
+    <div class="row" style="height: 100vh;">
+        <div class="col-12">
+            <div class="header-menu-container">
+                <div class="logo-container">
+                    <img src="{{ asset('images/logo.jpeg') }}" alt="Logo Mobile Hub" style="max-height: 150px;">
                 </div>
-                    <div class="col-10">
-                        <div class="container">
-                        @section("content")
-                        @show
-                        </div>
-                    </div>
+                <div class="menu-bar">
+                    <a href="{{ url('/catalogos/servicios') }}" class="nav-link">Servicios</a>
+                    <a href="{{ url('/catalogos/accesorios') }}">Accesorios</a>
+                    <a href="{{ url('/catalogos/ventas') }}">Ventas</a>
+                    <a href="{{ url('/reportes/') }}">Reportes</a>
+                    <a href="{{ url('/logout') }}">Salir</a>
                 </div>
             </div>
+            <div class="container">
+                @section("content")
+                @show
+            </div>
         </div>
-    </body>
+    </div>
+</body>
 </html>
