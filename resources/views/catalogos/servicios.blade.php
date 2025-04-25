835b35413a553563d5b6012a114ef7065f22a7de
emiliano53 <emiliano.santos.flores@gmail.com>
2025-04-24 20:11:53 -0600

    servicios

--- resources/views/catalogos/servicios.blade.php
+++ resources/views/catalogos/servicios.blade.php
@@ -5,9 +5,9 @@
     <div class="container">
         <h1>Lista de Servicios</h1>
         <div class="col-auto titlebar-commands">
-            <a class="btn btn-primary" href="{{url('/catalogos/servicios/create')}}">Agregar</a>
+            <a class="btn btn-primary" href="{{ url('/catalogos/servicios/create') }}">Agregar</a>
         </div>
-        
+
         <table class="table table-striped">
             <thead>
                 <tr>
@@ -24,9 +24,8 @@
                         <td>${{ number_format($servicio->costo, 2) }}</td>
                         <td>{{ $servicio->estado }}</td>
                         <td>
-                            
-                            <a href="{{url('/catalogos/servicios/edit', $servicio->id_servicio) }}" class="btn btn-warning btn-sm">Editar</a>
-                            <form action="{{ url('/catalogos/servicios.destroy', $servicio->id_servicio) }}" method="POST" style="display:inline;">
+                            <a href="{{ url('/catalogos/servicios/' . $servicio->id_servicio . '/edit') }}" class="btn btn-warning btn-sm">Editar</a>
+                            <form action="{{ url('/catalogos/servicios/' . $servicio->id_servicio) }}" method="POST" style="display:inline;">
                                 @csrf
                                 @method('DELETE')
                                 <button type="submit" class="btn btn-danger btn-sm">Eliminar</button>
